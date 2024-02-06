1f3ec323924dfa6e3404686ac22c406eac7f665a
SamuelAlvarezT <samuesteban70@gmail.com>
2024-02-06 15:58:13 -0500

    se crea coal de mensajes,se agraga color los mensajes que aparezen en la terminal

--- storage/main.py
+++ storage/main.py
@@ -3,6 +3,8 @@ import pyautogui
 import pyscreeze
 import asyncio
 import queue
+from colorama import init, Fore, Back, Style
+
 
 # 'tu_api_id' y 'tu_api_hash'
 api_id = '28089555'
@@ -11,32 +13,35 @@ api_hash = '1ebe2cac78e82f0bbcb3cb78f6248229'
 # Lista para almacenar los mensajes filtrados
 mensajes_filtrados = []
 
-# Crea una cola para almacenar los mensajes
-cola_de_mensajes = queue.Queue()
-
 #Inicia el cliente de Telegram
-client = TelegramClient('24Intento', api_id, api_hash)
+client = TelegramClient('32Intento', api_id, api_hash)
+
+# Crea una cola y un set para almacenar los mensajes
+cola_de_mensajes = queue.Queue()
+mensajes_set = set()
 
 @client.on(events.NewMessage(chats=['@Crypto_Box_Code_Binance']))
 async def nuevo_mensaje(event):
     # This code will run each time a new message arrives in the group
     mensaje = event.message.message
-    print(f'Mensaje recibido: {mensaje}')
+    print(Fore.WHITE + f'Nuevo Mensaje recibido ')
     # Filtra los mensajes basándote en la cantidad de caracteres y si ya ha sido procesado
     num_caracteres = len(mensaje)
     if num_caracteres > 8:  # Cambia este número a la cantidad de caracteres que desees
-        print('El mensaje es demasiado largo, ignorándolo...')
+        print(Fore.CYAN + 'El mensaje es demasiado largo, ignorándolo...')
         return
     elif num_caracteres < 8:  # Asegura que el mensaje tenga al menos 8 caracteres
-        print('El mensaje es demasiado corto, ignorándolo...')
+        print(Fore.CYAN + 'El mensaje es demasiado corto, ignorándolo...')
         return
-    elif mensaje in mensajes_filtrados:
-        print('El mensaje ya ha sido procesado, ignorándolo.')
+    elif mensaje in mensajes_set:
+        print(Fore.CYAN + 'El mensaje ya ha sido procesado, ignorándolo...')
         return
-    # Si el mensaje pasa los filtros, añádelo a la cola
+    # Si el mensaje pasa los filtros, añádelo a la cola y al set
     cola_de_mensajes.put(mensaje)
-    await asyncio.sleep(1)
-    print('finalizando...')
+    mensajes_set.add(mensaje)
+    print(Fore.GREEN + f'mensaje aprobado {mensaje}')
+    await asyncio.sleep(0.1)
+
     
 
 async def procesar_mensajes():
@@ -69,8 +74,8 @@ async def procesar_mensajes():
                     print("Imagen 'AbrirCaja.png' no encontrada. Intentando de nuevo.")
                     x, y = pyscreeze.locateCenterOnScreen('storage/ClosePacketDetails.png')
                     pyautogui.click(x, y)    
-        await asyncio.sleep(1)    
-        print('finalizando...')
+        await asyncio.sleep(0.1)    
+        
 
 # Inicia el cliente y el procesador de mensajes
 with client:
