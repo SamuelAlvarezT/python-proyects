0eb5b8a8a7f35080c5df35faf8427426f576bdf5
SamuelAlvarezT <samuesteban70@gmail.com>
2024-02-09 07:30:54 -0500

    se crea un json para guradar los codigos aparte de ahora registrara quien lo mando y a que horas,se hacen cambios importantes al codigo main.py se cambia el preceso de filtrado de codigo, y se agrugan algunos try parta el manejio de excepciones

--- storage/main.py
+++ storage/main.py
@@ -3,7 +3,8 @@ import pyautogui
 import pyscreeze
 import asyncio
 import queue
-from colorama import init, Fore, Back, Style
+import json
+from colorama import Fore
 
 
 # 'tu_api_id' y 'tu_api_hash'
@@ -14,68 +15,90 @@ api_hash = '1ebe2cac78e82f0bbcb3cb78f6248229'
 mensajes_filtrados = []
 
 #Inicia el cliente de Telegram
-client = TelegramClient('32Intento', api_id, api_hash)
+client = TelegramClient('42Intento', api_id, api_hash)
 
 # Crea una cola y un set para almacenar los mensajes
 cola_de_mensajes = queue.Queue()
 mensajes_set = set()
 
+# Ruta del archivo JSON para almacenar los mensajes
+json_file_path = 'mensajes.json'
+# Verifica si el archivo JSON ya existe
+try:
+    with open(json_file_path, 'r') as file:
+        mensajes_filtrados = json.load(file)
+except FileNotFoundError:
+    # Si el archivo no existe, inicializa mensajes_filtrados como una lista vacía
+    mensajes_filtrados = []
+
+
 @client.on(events.NewMessage(chats=['@Crypto_Box_Code_Binance']))
 async def nuevo_mensaje(event):
-    # This code will run each time a new message arrives in the group
     mensaje = event.message.message
-    print(Fore.WHITE + f'Nuevo Mensaje recibido ')
-    # Filtra los mensajes basándote en la cantidad de caracteres y si ya ha sido procesado
+    hora_recepcion = event.date.strftime("%Y-%m-%d %H:%M:%S")  # Obtiene la hora de recepción del mensaje
+    # Obtiene el remitente del mensaje
+    remitente = event.sender.username if event.sender and event.sender.username else event.sender.first_name if event.sender else 'Desconocido'  
+
     num_caracteres = len(mensaje)
-    if num_caracteres > 8:  # Cambia este número a la cantidad de caracteres que desees
-        print(Fore.CYAN + 'El mensaje es demasiado largo, ignorándolo...')
+    if num_caracteres > 8 or num_caracteres < 8 or mensaje in mensajes_filtrados or '/' in mensaje or ' ' in mensaje:
         return
-    elif num_caracteres < 8:  # Asegura que el mensaje tenga al menos 8 caracteres
-        print(Fore.CYAN + 'El mensaje es demasiado corto, ignorándolo...')
-        return
-    elif mensaje in mensajes_set:
-        print(Fore.CYAN + 'El mensaje ya ha sido procesado, ignorándolo...')
-        return
-    # Si el mensaje pasa los filtros, añádelo a la cola y al set
-    cola_de_mensajes.put(mensaje)
+
+    mensaje_info = {
+        "mensaje": mensaje,
+        "hora_recepcion": hora_recepcion,
+        "remitente": remitente
+    }
+
+    cola_de_mensajes.put(mensaje_info)
     mensajes_set.add(mensaje)
-    print(Fore.GREEN + f'mensaje aprobado {mensaje}')
+    mensajes_filtrados.append((mensaje_info)),("\n")
+
+
+    with open(json_file_path, 'w') as file:
+        json.dump(mensajes_filtrados, file)
+
+    print(Fore.GREEN + f'Mensaje aprobado: {mensaje}')
     await asyncio.sleep(0.1)
 
-    
 
 async def procesar_mensajes():
     while True:
         if not cola_de_mensajes.empty():
-            mensaje = cola_de_mensajes.get()
-            print('Arrancando')
-            x, y = pyscreeze.locateCenterOnScreen('storage/IrBinance.png')
-            pyautogui.click(x, y)
-            print("UBICADO EN BINANCE")
-            pyautogui.click('storage/NewPaste.png')
-            pyautogui.write (mensaje)
-            print("Texto pegado de protapales")
+            mensaje_info = cola_de_mensajes.get()
+            mensaje = mensaje_info["mensaje"]
 
             try:
-                x, y = pyscreeze.locateCenterOnScreen('storage/ClaimNow.png')
+                print('Arrancando')
+                x, y = pyscreeze.locateCenterOnScreen('storage/IrBinance.png')
                 pyautogui.click(x, y)
-            except pyscreeze.ImageNotFoundException:
-                x, y = pyscreeze.locateCenterOnScreen('storage/CapturaRecargar.png')
-                pyautogui.click(x, y)
+                print("UBICADO EN BINANCE")
+                pyautogui.click('storage/NewPaste.png')
+                pyautogui.write(mensaje)
+                print("Texto pegado de protapales")
 
-            for _ in range(3):  # Intenta hacer clic tres veces en 'AbrirCaja.png'
                 try:
-                    pyautogui.click('storage/Open.png')
-                    print("crytptobox abierta")  # Sale del bucle si el clic es exitoso
+                    x, y = pyscreeze.locateCenterOnScreen('storage/ClaimNow.png')
+                    pyautogui.click(x, y)
                 except pyscreeze.ImageNotFoundException:
-                    print("Imagen 'AbrirCaja.png' no encontrada. Intentando de nuevo.")
                     x, y = pyscreeze.locateCenterOnScreen('storage/CapturaRecargar.png')
                     pyautogui.click(x, y)
-                    print("Imagen 'AbrirCaja.png' no encontrada. Intentando de nuevo.")
-                    x, y = pyscreeze.locateCenterOnScreen('storage/ClosePacketDetails.png')
-                    pyautogui.click(x, y)    
-        await asyncio.sleep(0.1)    
-        
+
+                for _ in range(3):
+                    try:
+                        pyautogui.click('storage/Open.png')
+                        print("Crytptobox abierta")
+                        break
+                    except pyscreeze.ImageNotFoundException:
+                        print("Imagen 'AbrirCaja.png' no encontrada. Intentando de nuevo.")
+                        x, y = pyscreeze.locateCenterOnScreen('storage/CapturaRecargar.png')
+                        pyautogui.click(x, y)
+                        print("Imagen 'AbrirCaja.png' no encontrada. Intentando de nuevo.")
+                        x, y = pyscreeze.locateCenterOnScreen('storage/ClosePacketDetails.png')
+                        pyautogui.click(x, y)
+            except pyscreeze.ImageNotFoundException:
+                print(Fore.RED + f"imagen no encontrada, finalizando ")
+        await asyncio.sleep(0.1)
+
 
 # Inicia el cliente y el procesador de mensajes
 with client:
